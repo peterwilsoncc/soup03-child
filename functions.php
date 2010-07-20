6a6a434dca5b569d82976213f67c82c354c20c92
Peter Wilson <wilson@peterwilson.cc>
2010-07-20 13:38:26 +1000

    Tidy up ordering actions

--- functions.php
+++ functions.php
@@ -22,7 +22,6 @@ function soup_setupChildThemeClass() {
 			* registerMenus
 		*/
 	
-	
 	}
 
 } // function soup_setupChildThemeClass() 
@@ -32,9 +31,9 @@ function soup_setupChildThemeClass() {
 	need to reverse the order the function.php files usually run in
 	parent's function.php needs to run before child's
 */
-//add_action('init', 'soup_setupParentThemeClass', 1); //reference: runs in parents's function.php
-add_action('init', 'soup_setupChildThemeClass', 2); 
-//add_action('init', 'soup_initialiseSoupObject', 3); //reference: runs in parents's function.php
+//add_action('after_setup_theme', 'soup_setupParentThemeClass', 1); //reference: runs in parents's function.php
+add_action('after_setup_theme', 'soup_setupChildThemeClass', 2); 
+//add_action('after_setup_theme', 'soup_initialiseSoupObject', 3); //reference: runs in parents's function.php
 
 
 ?>
